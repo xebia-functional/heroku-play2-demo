c6f4d0833f2882b29df8335f9b2f044695d1d4d0
Raúl Raja Martínez <raulraja@gmail.com>
2014-01-28 10:20:35 +0100

    added cache

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,25 +6,30 @@ import play.api.libs.json._
 
 import play.modules.reactivemongo.MongoController
 import play.modules.reactivemongo.json.collection.JSONCollection
+import play.api.cache.Cached
 
 
 object Application extends Controller with MongoController {
 
   def collection: JSONCollection = db.collection[JSONCollection]("persons")
 
-  def index = Action.async {
+  def index = Cached("index") {
+    Action.async {
 
-    val futurePeople = collection
-      .find(Json.obj())
-      .sort(Json.obj("created" -> -1))
-      .cursor[JsObject]
-      .collect[List](upTo = 50)
+      val futurePeople = collection
+        .find(Json.obj())
+        .sort(Json.obj("created" -> -1))
+        .cursor[JsObject]
+        .collect[List](upTo = 50)
 
-    for {
-      people <- futurePeople
-    } yield Ok(Json.obj("people" -> people))
+      for {
+        people <- futurePeople
+      } yield Ok(Json.obj("people" -> people))
 
+    }
   }
 
-  def loaderio() = Action { Ok ("loaderio-92f1a8a29de94e0b0ba549a0860ec0de")}
+  def loaderio() = Action {
+    Ok("loaderio-92f1a8a29de94e0b0ba549a0860ec0de")
+  }
 }
